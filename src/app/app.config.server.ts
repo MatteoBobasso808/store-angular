5f2a359c57494152c04375dcc69be30a1d100db2
MatteoBobasso808 <mbobasso@fi.uba.ar>
2025-01-12 16:30:46 -0300

    cambios

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,14 +1,16 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRoutesConfig } from '@angular/ssr';
+import { provideClientHydration } from '@angular/platform-browser';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRoutesConfig(serverRoutes)
-  ]
+    provideServerRoutesConfig(serverRoutes),
+    provideClientHydration(), // Agregado para habilitar la hidratación
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
+import {Product} from '../../../shared/models/product.module';
 
 @Component({
   selector: 'app-list',
-  imports: [ProductComponent],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor(){
+    const initProducts: Product[]= [
+      {
+        id: Date.now(),
+        title: 'Producto 1',
+        price: 100,
+        img: 'https://picsum.photos/640/640?r=2'
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 2',
+        price: 200,
+        img: 'https://picsum.photos/640/640?r=5'
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 3',
+        price: 200,
+        img: 'https://picsum.photos/640/640?r=10'
+      }
+    ];
+    this.products.set(initProducts);
+  }
+
   fromChild(event: string){
     console.log('Estamos en el padre')
     console.log(event)
